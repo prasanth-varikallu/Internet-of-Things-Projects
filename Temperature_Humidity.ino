1a22874c4dfd178d77f7c7e01e23dcce6f836a9f
Prasanth Varikallu <prasanth.varikallu@yahoo.in>
2016-02-09 23:07:19 -0500

    Create Temperature_Humidity.ino

---
+++ Temperature_Humidity.ino
@@ -0,0 +1,79 @@
+// Example testing sketch for various DHT humidity/temperature sensors
+// Written by ladyada, public domain
+
+#include "DHT.h"
+
+#define DHTPIN 5     // what digital pin we're connected to
+
+// Uncomment whatever type you're using!
+#define DHTTYPE DHT11   // DHT 11
+
+DHT dht(DHTPIN, DHTTYPE);
+
+void setup() {
+  Serial.begin(9600);
+  Serial.println("DHTxx test!");
+  pinMode(12, OUTPUT);
+  pinMode(13, OUTPUT);
+  pinMode(15, OUTPUT);
+
+  dht.begin();
+}
+
+void loop() {
+  // Wait a few seconds between measurements.
+  delay(2000);
+
+  // Reading temperature or humidity takes about 250 milliseconds!
+  // Sensor readings may also be up to 2 seconds 'old' (its a very slow sensor)
+  float h = dht.readHumidity();
+  // Read temperature as Celsius (the default)
+  float t = dht.readTemperature();
+  // Read temperature as Fahrenheit (isFahrenheit = true)
+  float f = dht.readTemperature(true);
+
+  // Check if any reads failed and exit early (to try again).
+  if (isnan(h) || isnan(t) || isnan(f)) {
+    Serial.println("Failed to read from DHT sensor!");
+    digitalWrite(12, HIGH);
+    digitalWrite(13, LOW);
+    digitalWrite(15, LOW);
+    return;
+  }
+
+  // Compute heat index in Fahrenheit (the default)
+  float hif = dht.computeHeatIndex(f, h);
+  // Compute heat index in Celsius (isFahreheit = false)
+  float hic = dht.computeHeatIndex(t, h, false);
+
+  Serial.print("Humidity: ");
+  Serial.print(h);
+  Serial.print(" %\t");
+  Serial.print("Temperature: ");
+  Serial.print(t);
+  Serial.print(" *C ");
+  Serial.print(f);
+  Serial.print(" *F\t");
+  Serial.print("Heat index: ");
+  Serial.print(hic);
+  Serial.print(" *C ");
+  Serial.print(hif);
+  Serial.println(" *F");
+
+  if (hif >= 50 && hif< 60) {
+    digitalWrite(15, HIGH);
+    digitalWrite(12, LOW);
+  }
+
+  else if (hif >= 60 && hif< 70) {
+    digitalWrite(15, HIGH);
+    digitalWrite(13, HIGH);
+    digitalWrite(12, LOW);
+  }
+
+  else {
+    digitalWrite(15, LOW);
+    digitalWrite(13, LOW);
+    digitalWrite(12, HIGH);
+  }
+}
